bf8dc92397cf333a80ed27ae8563fb3b88000618
duhanugurlu <94967492+duhanugurlu@users.noreply.github.com>
2021-11-25 19:44:13 +0300

    Add files via upload
    v1.0.1

--- homework.py
+++ homework.py
@@ -1,4 +1,9 @@
-side_length = int(input("Side length of square: "))
-total_movement = 2*side_length
+import argparse
+
+parser = argparse.ArgumentParser(description="Total number of path finding, input a positive integer")
+parser.add_argument("-integer", default=2)
+args = parser.parse_args()
+total_movement = 2*int(args.integer)
 num_variations = (total_movement-1)*2
+
 print(num_variations)
